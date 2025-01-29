d23f3763a3a72798d0bc6c0313a5cee303a498cc
bjheinen <benedict.heinen@gmail.com>
2025-01-29 13:09:05 +0000

    Add compute functions to override in derived classes

--- include/burnman/classes/material.hpp
+++ include/burnman/classes/material.hpp
@@ -25,6 +25,240 @@ class Material {
   // Virtual destructor may be needed for cleanup in derived classes
   virtual ~Material() = default;
 
+ protected:
+
+  // protected compute functions to override in derived classes
+  /**
+   * @brief Computes the molar internal energy of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Internal energy in [J/mol].
+   * @throws NotImplemntedError if default implementation called.
+   */
+  virtual double compute_molar_internal_energy() const;
+
+  /**
+   * @brief Computes the molar Gibbs free energy of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Gibbs free energy in [J/mol].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_molar_gibbs() const;
+
+  /**
+   * @brief Computes the molar Helmholtz free energy of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Helmholtz free energy in [J/mol].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_molar_helmholtz() const;
+
+  /**
+   * @brief Computes the molar mass of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Molar mass in [kg/mol].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_molar_mass() const;
+
+    /**
+   * @brief Computes the molar volume of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Molar volume in [m^3/mol].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_molar_volume() const;
+
+  /**
+   * @brief Computes the density of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Density in [kg/m^3].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_density() const;
+
+  /**
+   * @brief Computes the molar entropy of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Entropy in [J/K/mol].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_molar_entropy() const;
+
+  /**
+   * @brief Computes the molar enthalpy of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Enthalpy in [J/mol].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_molar_enthalpy() const;
+
+  /**
+   * @brief Computes the isothermal bulk modulus of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Isothermal bulk modulus in [Pa].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_isothermal_bulk_modulus_reuss() const;
+
+  /**
+   * @brief Computes the isentropic bulk modulus of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Isentropic bulk modulus in [Pa].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_isentropic_bulk_modulus_reuss() const;
+  
+  /**
+   * @brief Computes the isothermal compressibility of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Isothermal compressibility in [1/Pa].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_isothermal_compressibility_reuss() const;
+
+  /**
+   * @brief Computes the isentropic compressibility of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Adiabatic compressibility in [1/Pa].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_isentropic_compressibility_reuss() const;
+
+  /**
+   * @brief Computes the shear modulus of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Shear modulus in [Pa].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_shear_modulus() const;
+
+  /**
+   * @brief Computes the P wave velocity of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return P wave velocity in [m/s].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_p_wave_velocity() const;
+
+  /**
+   * @brief Computes the bulk sound velocity of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Bulk sound velocity in [m/s].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_bulk_sound_velocity() const;
+
+  /**
+   * @brief Computes shear wave velocity the  of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Shear wave velocity in [m/s].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_shear_wave_velocity() const;
+
+  /**
+   * @brief Computes the grueneisen parameter of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Grueneisen parameter [unitless].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_grueneisen_parameter() const;
+
+  /**
+   * @brief Computes the thermal expansion coefficient of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Thermal expansivity in [1/K].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_thermal_expansivity() const;
+
+  /**
+   * @brief Computes the molar heat capacity at constant volume of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Isochoric heat capacity in [J/K/mol].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_molar_heat_capacity_v() const;
+
+  /**
+   * @brief Computes molar heat capacity at constant pressure of the mineral.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return Isobaric heat capacity in [J/K/mol].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_molar_heat_capacity_p() const;
+
+  /**
+   * @brief Computes change in temperature with pressure at constant entropy.
+   *
+   * @note Default implementation throws NotImplementedError.
+   *       Derived classes should override method.
+   *
+   * @return dTdP at constant entropy in [Pa/K].
+   * @throws NotImplementedError if default implementation called.
+   */
+  virtual double compute_isentropic_thermal_gradient() const;
+
 
  private:
   // std::optional used for caching
