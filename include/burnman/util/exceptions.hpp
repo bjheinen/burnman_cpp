4fcca5558a74e1217dc16bd565ad95f41f763386
bjheinen <benedict.heinen@gmail.com>
2025-01-29 13:37:04 +0000

    Custom NotImplementedError exception for use in base class default implementations

---
+++ include/burnman/util/exceptions.hpp
@@ -0,0 +1,24 @@
+/*
+  TODO: Copyright Notice!
+*/
+#ifndef BURNMAN_UTIL_EXCEPTIONS_HPP_INCLUDED
+#define BURNMAN_UTIL_EXCEPTIONS_HPP_INCLUDED
+
+#include <stdexcept>
+#include <string>
+
+/**
+ * Custom exception derived from std::logic_error
+ * Usage: throw NotImplementedError(class_name, func_name)
+ *        class_name from e.g. typeid(*this).name();
+ *        func_name from e.g. __func__ identifier
+ */
+class NotImplementedError : public std::logic_error {
+ public:
+  explicit NotImplementedError(const std::string& class_name,
+                               const std::string& func_name,
+                               const std::string& message = "Function not implemented!")
+    : std::logic_error("[" + class_name + "::" + func_name + "] " + message) {}
+};
+
+#endif  // BURNMAN_UTIL_EXCEPTIONS_HPP_INCLUDED
