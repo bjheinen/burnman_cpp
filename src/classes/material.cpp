ed2f871069a25eddbc828e11318ca02d73549a11
bjheinen <benedict.heinen@gmail.com>
2025-01-29 15:03:27 +0000

    Fix includes - static for now only

--- src/classes/material.cpp
+++ src/classes/material.cpp
@@ -2,8 +2,8 @@
   TODO: Copyright Notice!
 */
 #include <typeinfo>
-#include "../include/burnman/classes/material.hpp"
-#include "../include/burnman/util/exceptions.hpp"
+#include "../../include/burnman/classes/material.hpp"
+#include "../../include/burnman/util/exceptions.hpp"
 
 [[noreturn]] void Material::throw_not_implemented_error(const std::string& method) const {
   throw NotImplementedError(get_class_name(), method);
@@ -12,7 +12,6 @@
 std::string Material::get_class_name() const {
   return typeid(*this).name();
 }
-
 
 void Material::reset() {
   molar_internal_energy.reset();
