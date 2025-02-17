0baf59d5dc1580e26b27bbc487fc44c1d7f111d3
bjheinen <benedict.heinen@gmail.com>
2025-02-17 20:45:11 +0000

    Add MGD EOS

---
+++ include/burnman/eos/mie_grueneisen_debye.hpp
@@ -0,0 +1,249 @@
+/*
+  TODO: Copyright Notice!
+*/
+#ifndef BURNMAN_EOS_MIE_GRUENEISEN_DEBYE_HPP_INCLUDED
+#define BURNMAN_EOS_MIE_GRUENEISEN_DEBYE_HPP_INCLUDED
+
+#include "burnman/core/equation_of_state.hpp"
+#include "burnman/utils/eos.hpp"
+
+/**
+ * @class MGD3
+ * @brief Class for the Mie-Grueneisen-Debye equation of state.
+ *
+ * Uses finite-strain Birch-Murnaghan for the isothermal portion.
+ * References can be found in many places, e.g.
+ *   Shim, Duffy and Kenichi (2002)
+ *   Jackson and Rigden (1996)
+ *   Matas et al. (2007) Appendices
+ * The thermal correction to the shear modulus is from Hama and Suito (1998).
+ * Uses third order expansion also for shear modulus. To fit shear modulus
+ * to second order expansion @see MGD2
+ * 
+ * @note All functions assume SI units for all properties.
+ */
+class MGD3 : public EquationOfState{
+ public:
+
+  // Helper functions
+  bool validate_parameters(MineralParams& params) override;
+
+  // Specific EOS functions
+  /**
+   * @copydoc EquationOfState::compute_volume
+   *
+   * @note Matas et al. eq. B7.
+   */
+  double compute_volume(
+    double pressure,
+    double temperature,
+    const MineralParams& params) const override;
+
+  /**
+   * @copydoc EquationOfState::compute_pressure
+   *
+   * @note Matas et al. eq. B7.
+   */
+  double compute_pressure(
+    double temperature,
+    double volume,
+    const MineralParams& params) const override;
+
+  double compute_grueneisen_parameter(
+    double pressure,
+    double temperature,
+    double volume,
+    const MineralParams& params) const override;
+
+  /**
+   * @copydoc EquationOfState::compute_isothermal_bulk_modulus_reuss
+   *
+   * @note Matas et al. eq. B8, B13.
+   */
+  double compute_isothermal_bulk_modulus_reuss(
+    double pressure,
+    double temperature,
+    double volume,
+    const MineralParams& params) const override;
+
+  /**
+   * @copydoc EquationOfState::compute_isentropic_bulk_modulus_reuss
+   *
+   * @note Matas et al. eq. D6.
+   */
+  double compute_isentropic_bulk_modulus_reuss(
+    double pressure,
+    double temperature,
+    double volume,
+    const MineralParams& params) const override;
+
+  /**
+   * @copydoc EquationOfState::compute_shear_modulus
+   *
+   * @note Third order expansion, Matas et al. eq. B11.
+   */
+  double compute_shear_modulus(
+    double pressure,
+    double temperature,
+    double volume,
+    const MineralParams& params) const override;
+
+  double compute_molar_heat_capacity_v(
+    double pressure,
+    double temperature,
+    double volume,
+    const MineralParams& params) const override;
+
+  double compute_molar_heat_capacity_p(
+    double pressure,
+    double temperature,
+    double volume,
+    const MineralParams& params) const override;
+
+  double compute_thermal_expansivity(
+    double pressure,
+    double temperature,
+    double volume,
+    const MineralParams& params) const override;
+
+  double compute_gibbs_free_energy(
+    double pressure,
+    double temperature,
+    double volume,
+    const MineralParams& params) const override;
+
+  double compute_entropy(
+    double pressure,
+    double temperature,
+    double volume,
+    const MineralParams& params) const override;
+
+  double compute_molar_internal_energy(
+    double pressure,
+    double temperature,
+    double volume,
+    const MineralParams& params) const override;
+
+  double compute_helmholtz_free_energy(
+    double pressure,
+    double temperature,
+    double volume,
+    const MineralParams& params) const override;
+
+  double compute_enthalpy(
+    double pressure,
+    double temperature,
+    double volume,
+    const MineralParams& params) const override;
+
+ protected:
+
+  /**
+   * @brief Thermal correction to shear modulus
+   *
+   * @param temperature Temperature to evaluate [K].
+   * @param volume Volume to evaluate [m^3].
+   * @param params Mineral parameters object of type MineralParams.
+   *
+   * @return G_thermal [Pa].
+   */
+  double compute_thermal_shear_modulus(
+    double temperature,
+    double volume,
+    const MineralParams& params) const;
+
+ private:
+  /**
+   * @brief Compute the thermal correction to the bulk modulus.
+   *
+   * @param temperature Temperature to evaluate [K].
+   * @param volume Volume to evaluate [m^3].
+   * @param params Mineral parameters object of type MineralParams.
+   *
+   * @return K_th in [Pa].
+   */
+  double compute_thermal_bulk_modulus(
+    double temperature,
+    double volume,
+    const MineralParams& params) const;
+
+  /**
+   * @brief Computes the Grueneisen parameter.
+   *
+   * Matas et al. eq. B6.
+   *
+   * @param x V_0/V.
+   * @param params Mineral parameters object of type MineralParams
+   * 
+   * @return Grueneisen parameter [unitless].
+   */
+  static double compute_mgd_grueneisen_parameter(
+    double x,
+    const MineralParams& params);
+
+  /**
+   * @brief Compute the Debye temperature.
+   *
+   * See Matas et al. eq. B6.
+   *
+   * @param x V_0/V (inverse compression).
+   * @param params Mineral parameters object of type MineralParams.
+   *
+   * @return Debye temperature in [K].
+   */
+  static double compute_debye_temperature(
+    double x,
+    const MineralParams& params);
+
+  /**
+   * @brief Compute the isotropic thermal pressure.
+   *
+   * See Matas et al. eq. B4.
+   *
+   * @param temperature Temperature to evaluate [K].
+   * @param volume Volume to evaluate [m^3].
+   * @param params Mineral parameters object of type MineralParams.
+   *
+   * @return Thermal pressure in [Pa].
+   */
+  static double compute_thermal_pressure(
+    double temperature,
+    double volume,
+    const MineralParams& params);
+
+  /**
+   * @brief GSL function wrapper to compute P(V) - P
+   * 
+   * @param x Volume to test (passed by solver)
+   * @param p Generic pointer for parameter object
+   * @see `ParamsGSL::SolverParams_P`
+   */
+  static double mgd_gsl_wrapper(double x, void* p);
+
+};
+
+
+/**
+ * @class BGD2
+ * @brief MGD EOS with second order expansion of shear modulus.
+ *
+ * Derived from MGD3.
+ * Overrides compute_shear_modulus to use the second order expansion.
+ *
+ * @note All functions assume SI units for all properties.
+ */
+class MGD2 : public MGD3{
+ public:
+  /**
+   * @copydoc EquationOfState::compute_shear_modulus
+   *
+   * @note Second order expansion
+   */
+  double compute_shear_modulus(
+    double pressure,
+    double temperature,
+    double volume,
+    const MineralParams& params) const override;
+};
+
+#endif // BURNMAN_EOS_MIE_GRUENEISEN_DEBYE_HPP_INCLUDED
---
+++ src/eos/mie_grueneisen_debye.cpp
@@ -0,0 +1,374 @@
+/*
+  TODO: Copyright Notice!
+*/
+#include <cmath>
+//#include <stdexcept>
+#include "burnman/eos/mie_grueneisen_debye.hpp"
+#include "burnman/eos/debye.hpp"
+#include "burnman/eos/birch_murnaghan.hpp"
+#include "burnman/optim/brent_volume_solver.hpp"
+#include "burnman/utils/constants.hpp"
+
+bool MGD3::validate_parameters(MineralParams& params) {
+  ;
+}
+
+// Compute P(V) - P as function to root find
+double MGD3::mgd_gsl_wrapper(double x, void* p) {
+  auto* mgd_params = static_cast<const ParamsGSL::SolverParams_PT*>(p);
+  return
+    BM3::compute_birch_murnaghan(
+      *mgd_params->params.V_0 / x,
+      mgd_params->params)
+    + compute_thermal_pressure(
+      mgd_params->temperature,
+      x,
+      mgd_params->params)
+    - compute_thermal_pressure(
+      *mgd_params->params.T_0,
+      x,
+      mgd_params->params)
+    - mgd_params->pressure;
+}
+
+double MGD3::compute_volume(
+  double pressure,
+  double temperature,
+  const MineralParams& params
+) const {
+  // Find root in [P(V) - P] to find V that fits P
+  // Set up GSL params struct - to pass to objective function
+  ParamsGSL::SolverParams_PT mgd_params{
+    params,
+    pressure,
+    temperature};
+  // Set a, b limits
+  double x_lo = 0.01 * (*params.V_0);
+  double x_hi = 1.5 * (*params.V_0);
+  double volume_root = brent::find_root(
+    &mgd_gsl_wrapper,
+    mgd_params,
+    x_lo,
+    x_hi
+  );
+  return volume_root;
+}
+
+
+double MGD3::compute_pressure(
+  double temperature,
+  double volume,
+  const MineralParams& params
+) const {
+  return BM3::compute_birch_murnaghan(
+    *params.V_0 / volume,
+    params)
+  + compute_thermal_pressure(temperature, volume, params)
+  - compute_thermal_pressure(*params.T_0, volume, params);
+}
+
+double MGD3::compute_grueneisen_parameter(
+  double pressure [[maybe_unused]],
+  double temperature [[maybe_unused]],
+  double volume,
+  const MineralParams& params
+) const {
+  return compute_mgd_grueneisen_parameter(
+    *params.V_0 / volume,
+    params);
+}
+
+double MGD3::compute_mgd_grueneisen_parameter(
+  double x,
+  const MineralParams& params
+) {
+  return *params.grueneisen_0 * std::pow(x, -*params.q_0);
+}
+
+double MGD3::compute_isothermal_bulk_modulus_reuss(
+  double pressure [[maybe_unused]],
+  double temperature [[maybe_unused]],
+  double volume,
+  const MineralParams& params
+) const {
+  return BM3::compute_bm_bulk_modulus(volume, params)
+    + compute_thermal_bulk_modulus(temperature, volume, params)
+    - compute_thermal_bulk_modulus(*params.T_0, volume, params);
+}
+
+double MGD3::compute_isentropic_bulk_modulus_reuss(
+  double pressure,
+  double temperature,
+  double volume,
+  const MineralParams& params
+) const {
+  double K_T = compute_isothermal_bulk_modulus_reuss(
+    pressure,
+    temperature,
+    volume,
+    params
+  );
+  double alpha = compute_thermal_expansivity(
+    pressure,
+    temperature,
+    volume,
+    params
+  );
+  double gamma = compute_mgd_grueneisen_parameter(
+    *params.V_0 / volume,
+    params);
+  return K_T * (1.0 + gamma * alpha * temperature);
+}
+
+// Second order (MGD2)
+double MGD2::compute_shear_modulus(
+  double pressure [[maybe_unused]],
+  double temperature,
+  double volume,
+  const MineralParams& params
+) const {
+  return BM2::compute_second_order_shear_modulus(volume, params)
+    + compute_thermal_shear_modulus(temperature, volume, params)
+    - compute_thermal_shear_modulus(*params.T_0, volume, params);
+}
+
+// Third order (MGD3)
+double MGD3::compute_shear_modulus(
+  double pressure [[maybe_unused]],
+  double temperature,
+  double volume,
+  const MineralParams& params
+) const {
+  return BM3::compute_third_order_shear_modulus(volume, params)
+    + compute_thermal_shear_modulus(temperature, volume, params)
+    - compute_thermal_shear_modulus(*params.T_0, volume, params);
+}
+
+double MGD3::compute_molar_heat_capacity_v(
+  double pressure [[maybe_unused]],
+  double temperature,
+  double volume,
+  const MineralParams& params
+) const {
+  double debye_temperature = compute_debye_temperature(
+    *params.V_0/volume,
+    params);
+  return debye::compute_molar_heat_capacity_v(
+    temperature,
+    debye_temperature,
+    *params.napfu
+  );
+}
+
+double MGD3::compute_molar_heat_capacity_p(
+  double pressure,
+  double temperature,
+  double volume,
+  const MineralParams& params
+) const {
+  double C_v = compute_molar_heat_capacity_v(
+    pressure,
+    temperature,
+    volume,
+    params
+  );
+  double gamma = compute_mgd_grueneisen_parameter(
+    *params.V_0 / volume,
+    params
+  );
+  double alpha = compute_thermal_expansivity(
+    pressure,
+    temperature,
+    volume,
+    params
+  );
+  return C_v * (1.0 + gamma * alpha * temperature);
+}
+
+double MGD3::compute_thermal_expansivity(
+  double pressure,
+  double temperature,
+  double volume,
+  const MineralParams& params
+) const {
+  double C_v = compute_molar_heat_capacity_v(
+    pressure,
+    temperature,
+    volume,
+    params
+  );
+  double gamma = compute_mgd_grueneisen_parameter(
+    *params.V_0 / volume,
+    params
+  );
+  double K = compute_isothermal_bulk_modulus_reuss(
+    pressure,
+    temperature,
+    volume,
+    params
+  );
+  return gamma * C_v / K / volume;
+}
+
+double MGD3::compute_gibbs_free_energy(
+  double pressure,
+  double temperature,
+  double volume,
+  const MineralParams& params
+) const {
+  return compute_helmholtz_free_energy(
+    pressure,
+    temperature,
+    volume,
+    params
+  )
+  + pressure * volume;
+}
+
+double MGD3::compute_entropy(
+  double pressure [[maybe_unused]],
+  double temperature [[maybe_unused]],
+  double volume [[maybe_unused]],
+  const MineralParams& params [[maybe_unused]]
+) const {
+  double debye_temperature = compute_debye_temperature(
+    *params.V_0 / volume,
+    params
+  );
+  return debye::compute_entropy(
+    temperature,
+    debye_temperature,
+    *params.napfu);
+}
+
+double MGD3::compute_molar_internal_energy(
+  double pressure [[maybe_unused]],
+  double temperature [[maybe_unused]],
+  double volume,
+  const MineralParams& params
+) const {
+  return compute_helmholtz_free_energy(
+    pressure,
+    temperature,
+    volume,
+    params)
+  + temperature
+  * compute_entropy(
+    pressure,
+    temperature,
+    volume,
+    params);
+}
+
+double MGD3::compute_helmholtz_free_energy(
+  double pressure [[maybe_unused]],
+  double temperature,
+  double volume,
+  const MineralParams& params
+) const {
+  double x = *params.V_0 / volume;
+  double x_cbrt = std::cbrt(x);
+  double f = 0.5 * ((x_cbrt*x_cbrt) - 1.0);
+  // Eq. 28/29 in SLB2005
+  double b_iikk = 9.0 * (*params.K_0);
+  double b_iikkmm = 27.0 * (*params.K_0) * (*params.Kprime_0 - 4.0);
+  constexpr double one_sixth = 1.0 / 6.0;
+  double F_pressure =
+    0.5 * b_iikk * f * f * (*params.V_0)
+    + one_sixth * (*params.V_0) * b_iikkmm * f * f * f;
+  double debye_temperature = compute_debye_temperature(x, params);
+  double F_thermal =
+    debye::compute_helmholtz_free_energy(
+      temperature, debye_temperature, *params.napfu)
+    - debye::compute_helmholtz_free_energy(
+      *params.T_0, debye_temperature, *params.napfu);
+  return *params.F_0 + F_pressure + F_thermal;
+}
+
+double MGD3::compute_enthalpy(
+  double pressure,
+  double temperature,
+  double volume,
+  const MineralParams& params
+) const {
+  return compute_helmholtz_free_energy(
+    pressure,
+    temperature,
+    volume,
+    params)
+  + temperature
+  * compute_entropy(
+    pressure,
+    temperature,
+    volume,
+    params)
+  + pressure * volume;
+}
+
+double MGD3::compute_thermal_shear_modulus(
+  double temperature,
+  double volume,
+  const MineralParams& params
+) const {
+  if (temperature > 1.0e-10) {
+    double x = *params.V_0 / volume;
+    double gamma = compute_mgd_grueneisen_parameter(x, params);
+    double debye_temperature = compute_debye_temperature(x, params);
+    return 0.6 * (
+      compute_thermal_bulk_modulus(temperature, volume, params)
+      - 6.0 * constants::physics::gas_constant * temperature * (*params.napfu)
+      / volume
+      * gamma
+      * debye::debye_fn_cheb(debye_temperature / temperature)
+      );
+  } else {
+    return 0.0;
+  }
+}
+
+double MGD3::compute_debye_temperature(
+  double x,
+  const MineralParams& params
+) {
+  double gamma_diff = *params.grueneisen_0
+    - compute_mgd_grueneisen_parameter(x,params);
+  return *params.debye_0
+    * std::exp(gamma_diff)
+    / *params.q_0;
+}
+
+double MGD3::compute_thermal_pressure(
+  double temperature,
+  double volume,
+  const MineralParams& params
+) {
+  double x = *params.V_0 / volume;
+  double debye_temperature = compute_debye_temperature(x, params);
+  double gamma = compute_mgd_grueneisen_parameter(x, params);
+  double E_th = debye::compute_thermal_energy(
+    temperature,
+    debye_temperature,
+    *params.napfu);
+  return gamma * E_th / volume;
+}
+
+double MGD3::compute_thermal_bulk_modulus(
+  double temperature,
+  double volume,
+  const MineralParams& params
+) const {
+  if (temperature > 1.0e-10) {
+    double x = *params.V_0 / volume;
+    double gamma = compute_mgd_grueneisen_parameter(x, params);
+    double debye_temperature = compute_debye_temperature(x, params);
+    double xi = debye_temperature / temperature;
+    return 3.0 * (*params.napfu) * constants::physics::gas_constant
+      * temperature / volume * gamma
+      * (
+        (1.0 - (*params.q_0) - 3.0 * gamma) * debye::debye_fn_cheb(xi)
+        + 3.0 * gamma * xi / (std::exp(xi) - 1.0));
+  } else {
+    return 0.0;
+  }
+}
+
