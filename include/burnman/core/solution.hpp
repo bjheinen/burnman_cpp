da732a48b377876c3e6f26fe428c356bb707885b
bjheinen <benedict.heinen@gmail.com>
2025-02-20 17:03:38 +0000

    Add solution classes

---
+++ include/burnman/core/solution.hpp
@@ -0,0 +1,28 @@
+/*
+  TODO: Copyright Notice!
+*/
+#ifndef BURNMAN_CORE_SOLUTION_HPP_INCLUDED
+#define BURNMAN_CORE_SOLUTION_HPP_INCLUDED
+
+#include "burnman/core/mineral.hpp"
+
+/**
+ * Base class for solutions.
+
+  TODO
+  
+ */ 
+class Solution : public Mineral {
+
+ public:
+
+  virtual ~Solution() = default;
+
+ protected:
+  ;
+ private:
+  ;
+
+};
+
+#endif // BURNMAN_CORE_SOLUTION_HPP_INCLUDED
---
+++ include/burnman/core/solution_model.hpp
@@ -0,0 +1,65 @@
+/*
+  TODO: Copyright Notice!
+*/
+#ifndef BURNMAN_CORE_SOLUTION_MODEL_HPP_INCLUDED
+#define BURNMAN_CORE_SOLUTION_MODEL_HPP_INCLUDED
+
+/**
+ * Base class for solution models.
+
+  TODO
+  
+ */ 
+class SolutionModel {
+
+ public:
+
+  virtual ~SolutionModel() = default;
+
+ protected:
+  ;
+ private:
+  ;
+
+};
+
+#endif // BURNMAN_CORE_SOLUTION_MODEL_HPP_INCLUDED
+
+/*
+// Derived classes
+--> TODO: AsymmetricRegularSolution
+MechanicalSolution
+IdealSolution
+AsymmetricRegularSolution
+SymmetricRegularSolution --> special case of asym with alphas = ones
+SubRegularSolution
+FunctionSolution
+PolynomialSolution
+
+// Public getters and protected compute functions 
+// Make virtual where needed to override in derived classes
+excess_gibbs_free_energy
+excess_partial_gibbs_free_energies
+excess_volume
+excess_partial_volumes
+excess_entropy
+excess_partial_entropies
+excess_enthalpy
+Cp_excess
+alphaV_excess
+VoverKT_excess
+// In derived
+activity_coefficients
+activities
+// Asymmetric
+Constructor logic
+phi
+non_ideal_interactions
+non_ideal_excess_partial_gibbs
+excess_partial_gibbs_free_energies --> override
+excess_partial_entropies --> override
+excess_partial_volumes --> override
+gibbs_hessian
+entorpy_hessian
+volume_hessian
+*/
