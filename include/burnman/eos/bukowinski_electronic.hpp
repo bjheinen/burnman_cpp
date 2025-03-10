e9fe18b58cd0371d506503bece334c223fb7f170
bjheinen <benedict.heinen@gmail.com>
2025-03-10 19:40:24 +0000

    Add bukowinski electronic model

---
+++ include/burnman/eos/bukowinski_electronic.hpp
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2025 Benedict Heinen
+ *
+ * This file is part of burnman_cpp and is licensed under the
+ * GNU General Public License v3.0 or later. See the LICENSE file
+ * or <https://www.gnu.org/licenses/> for details.
+ *
+ * burnman_cpp is based on BurnMan: <https://geodynamics.github.io/burnman/>
+ */
+#ifndef BURNMAN_EOS_BUKOWINSKI_ELECTRONIC_HPP_INCLUDED
+#define BURNMAN_EOS_BUKOWINSKI_ELECTRONIC_HPP_INCLUDED
+
+#include "burnman/utils/eos.hpp"
+
+/**
+ * TODO: Add to EOS module
+ * @namespace bukowinski
+ * @brief Functions for the Bukowinski model for the electronic
+ *        component of the Helmholtz free energy (as used by
+ *        Stixrude & Lithgow-Bertelloni, 2024).
+ *
+ * @note All functions assume SI units for all properties.
+ */
+namespace bukowinski {
+  /**
+   * @brief Electronic component of Helmholtz free energy.
+   *
+   * @param temperature The temperature to evaluate [K].
+   * @param volume [m^3].
+   * @param params MineralParams object reference.
+   *
+   * @return Electronic component of Helmholtz free energy [J].
+   */
+  double compute_helmholtz_el(
+    double temperature,
+    double volume,
+    const MineralParams& params);
+
+  /**
+   * @brief Electronic component of pressure, -dF/dV.
+   *
+   * @param temperature The temperature to evaluate [K].
+   * @param volume [m^3].
+   * @param params MineralParams object reference.
+   *
+   * @return -dF/dV [Pa].
+   */
+  double compute_pressure_el(
+    double temperature,
+    double volume,
+    const MineralParams& params);
+
+  /**
+   * @brief Electronic component entropy, -dF/dT.
+   *
+   * @param temperature The temperature to evaluate [K].
+   * @param volume [m^3].
+   * @param params MineralParams object reference.
+   *
+   * @return -dF/dT [J/K].
+   */
+  double compute_entropy_el(
+    double temperature,
+    double volume,
+    const MineralParams& params);
+
+  /**
+   * @brief K_T / V, where K_T = -V dP/dV.
+   *
+   * @param temperature The temperature to evaluate [K].
+   * @param volume [m^3].
+   * @param params MineralParams object reference.
+   *
+   * @return K_T/V
+   */
+  double compute_KT_over_V(
+    double temperature,
+    double volume,
+    const MineralParams& params);
+
+  /**
+   * @brief C_v / T, where C_v = T dS/dT.
+   *
+   * @param temperature The temperature to evaluate [K].
+   * @param volume [m^3].
+   * @param params MineralParams object reference.
+   *
+   * @return dS/dT
+   */
+  double compute_CV_over_T(
+    double temperature,
+    double volume,
+    const MineralParams& params);
+
+  /**
+   * @brief alpha K_T = dP/dT.
+   *
+   * @param temperature The temperature to evaluate [K].
+   * @param volume [m^3].
+   * @param params MineralParams object reference.
+   *
+   * @return dP/dT
+   */
+  double compute_alpha_KT(
+    double temperature,
+    double volume,
+    const MineralParams& params);
+}
+
+#endif // BURNMAN_EOS_BUKOWINSKI_ELECTRONIC_HPP_INCLUDED
---
+++ src/eos/bukowinski_electronic.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2025 Benedict Heinen
+ *
+ * This file is part of burnman_cpp and is licensed under the
+ * GNU General Public License v3.0 or later. See the LICENSE file
+ * or <https://www.gnu.org/licenses/> for details.
+ *
+ * burnman_cpp is based on BurnMan: <https://geodynamics.github.io/burnman/>
+ */
+#include <cmath>
+#include "burnman/eos/bukowinski_electronic.hpp"
+
+double bukowinski::compute_helmholtz_el(
+  double temperature,
+  double volume,
+  const MineralParams& params
+) {
+  return -0.5 * (*params.bel_0)
+    * std::pow(volume / *params.V_0, *params.gel)
+    * (temperature * temperature - (*params.T_0) * (*params.T_0));
+}
+
+double bukowinski::compute_pressure_el(
+  double temperature,
+  double volume,
+  const MineralParams& params
+) {
+  return 0.5 * (*params.gel) * (*params.bel_0)
+    * std::pow(volume / *params.V_0, *params.gel)
+    * (temperature * temperature - (*params.T_0) * (*params.T_0))
+    / volume;
+}
+
+double bukowinski::compute_entropy_el(
+  double temperature,
+  double volume,
+  const MineralParams& params
+) {
+  return *params.bel_0 * temperature
+    * std::pow(volume / *params.V_0, *params.gel);
+}
+
+double bukowinski::compute_KT_over_V(
+  double temperature,
+  double volume,
+  const MineralParams& params
+) {
+  return -(*params.gel - 1.0)
+    * compute_pressure_el(temperature, volume, params)
+    / volume;
+}
+
+double bukowinski::compute_CV_over_T(
+  double temperature,
+  double volume,
+  const MineralParams& params
+) {
+  return *params.bel_0 * std::pow(volume / *params.V_0, *params.gel);
+}
+
+double bukowinski::compute_alpha_KT(
+  double temperature,
+  double volume,
+  const MineralParams& params
+) {
+  return *params.gel * (*params.bel_0) * temperature
+    * std::pow(volume / *params.V_0, *params.gel)
+    / volume;
+}
