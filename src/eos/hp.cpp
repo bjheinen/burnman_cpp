b58389504d2b7858c32e327c13001c243d20f43d
bjheinen <benedict.heinen@gmail.com>
2025-03-13 16:32:18 +0000

    Thermal additions to entropy and enthalpy

--- src/eos/hp.cpp
+++ src/eos/hp.cpp
@@ -11,6 +11,44 @@
 #include "burnman/eos/hp.hpp"
 #include "burnman/eos/einstein.hpp"
 
+double HP_TMT::compute_intCpdT(
+  double temperature,
+  const MineralParams& params
+) const {
+  double T_0 = *params.T_0;
+  CpParams cp_params = *params.Cp;
+  return
+    (
+      cp_params.a * temperature
+      + 0.5 * cp_params.b * temperature * temperature
+      - cp_params.c / temperature
+      + 2.0 * cp_params.d * std::sqrt(temperature))
+    - (
+      cp_params.a * T_0
+      + 0.5 * cp_params.b * T_0 * T_0
+      - cp_params.c / T_0
+      + 2.0 * cp_params.d * std::sqrt(T_0));
+}
+
+double HP_TMT::compute_intCpoverTdT(
+  double temperature,
+  const MineralParams& params
+) const {
+  CpParams cp_params = *params.Cp;
+  double T_0 = *params.T_0;
+  return 
+    (
+      cp_params.a * std::log(temperature)
+      + cp_params.b * temperature
+      - 0.5 * cp_params.c / (temperature * temperature)
+      - 2.0 * cp_params.d / std::sqrt(temperature))
+    - (
+      cp_params.a * std::log(T_0)
+      + cp_params.b * T_0
+      - 0.5 * cp_params.c / (T_0 * T_0)
+      - 2.0 * cp_params.d / std::sqrt(T_0));
+}
+
 double HP_TMT::compute_relative_thermal_pressure(
   double temperature,
   const MineralParams& params
