216f2832a8044c6f0b0c52ae2a475d6434cbf335
bjheinen <benedict.heinen@gmail.com>
2025-04-23 17:48:57 +0100

    Add utils namespace for grouping helper functions

---
+++ include/burnman/utils/utils.hpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025 Benedict Heinen
+ *
+ * This file is part of burnman_cpp and is licensed under the
+ * GNU General Public License v3.0 or later. See the LICENSE file
+ * or <https://www.gnu.org/licenses/> for details.
+ *
+ * burnman_cpp is based on BurnMan: <https://geodynamics.github.io/burnman/>
+ */
+#ifndef BURNMAN_UTILS_INCLUDED
+#define BURNMAN_UTILS_INCLUDED
+
+// Includes to allow #include burnman/utils
+#include "burnman/utils/string_utils.hpp"
+
+/**
+ * @namespace utils
+ * @brief General-purpose utility functions.
+ *
+ * Helpers for strings, maths, file I/O, etc.
+ */
+namespace utils {
+  // blank for documentation only
+}
+
+#endif // BURNMAN_UTILS_INCLUDED
