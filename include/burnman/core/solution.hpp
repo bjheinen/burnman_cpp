f08ffb9d543e844b00592869822af8696ce33a8e
bjheinen <benedict.heinen@gmail.com>
2025-05-12 17:31:03 +0100

    Add framework for Solution class

--- include/burnman/core/solution.hpp
+++ include/burnman/core/solution.hpp
@@ -10,24 +10,368 @@
 #ifndef BURNMAN_CORE_SOLUTION_HPP_INCLUDED
 #define BURNMAN_CORE_SOLUTION_HPP_INCLUDED
 
-#include "burnman/core/mineral.hpp"
+#include <Eigen/Dense>
+#include "burnman/core/material.hpp"
 
 /**
- * Base class for solutions.
-
-  TODO
-  
+ * @class Solution
+ * @brief Base class for solid solutions.
+ * 
+ * Base class for all solutions. 
+ * 
+ * Site occupancies, endmember activities and the constant
+ * pressure and temperature dependencies of the excess properties
+ * require set_composition().
+ * Solution states equire set_state().
+ * 
+ * Uses an instance of `SolutionModel' (or derived class) to calculate
+ * interaction terms between endmembers.
+ * 
+ * All solution parameters and properties are in SI units.
+ * e.g. Interaction parameters in J/mol, with T & P derivatives in J/K/mol
+ * and m^3/mol.
+ * 
  */ 
-class Solution : public Mineral {
+class Solution : public Material {
 
  public:
 
   virtual ~Solution() = default;
 
+  // Override of reset to include additional solution properties
+  void reset() override;
+
+  // Public getters for extra Solution functions
+  /**
+   * @brief Retrieves molar excess gibbs free energy the of the solid solution.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_excess_gibbs()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return Excess gibbs free energy in [J/mol].
+   */
+  double get_excess_gibbs() const;
+
+  /**
+   * @brief Retrieves the excess molar volume of the solid solution.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_excess_volume()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return Excess volume in [m^3/mol].
+   */
+  double get_excess_volume() const;
+
+  /**
+   * @brief Retrieves the excess molar entropy of the solid solution.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_excess_entropy()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return Excess entropy in [J/K/mol].
+   */
+  double get_excess_entropy() const;
+
+  /**
+   * @brief Retrieves the excess molar enthalpy of the solid solution.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_excess_enthalpy()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return Excess enthalpy in [J/mol].
+   */
+  double get_excess_enthalpy() const;
+
+  /**
+   * @brief Retrieves the endmember activities.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_activities()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return Endmember activities [unitless].
+   */
+  Eigen::ArrayXd get_activities() const;
+
+  /**
+   * @brief Retrieves endmember activity coefficients of the solid solution.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_activity_coefficients()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return (gamma = activity / ideal activity) [unitless].
+   */
+  Eigen::ArrayXd get_activity_coefficients() const;
+
+  /**
+   * @brief Retrieves the excess partial molar gibbs free energies of the solid solution.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_excess_partial_gibbs()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return Excess partial gibbs in [J/mol].
+   */
+  Eigen::ArrayXd get_excess_partial_gibbs() const;
+
+  /**
+   * @brief Retrieves the excess partial volumes of the solid solution.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_excess_partial_volumes()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return Excess partial volumes in [m^3/mol].
+   */
+  Eigen::ArrayXd get_excess_partial_volumes() const;
+
+  /**
+   * @brief Retrieves the excess partial entropies of the solid solution.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_excess_partial_entropies()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return Excess partial entropies in [J/K].
+   */
+  Eigen::ArrayXd get_excess_partial_entropies() const;
+
+  /**
+   * @brief Retrieves the endmember partial molar gibbs free energy.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_partial_gibbs()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return Partial molar gibbs free energies in [J/mol].
+   */
+  Eigen::ArrayXd get_partial_gibbs() const;
+
+  /**
+   * @brief Retrieves the endmember partial volumes of the solid solution.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_partial_volumes()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return Partial volumes in [m^3].
+   */
+  Eigen::ArrayXd get_partial_volumes() const;
+
+  /**
+   * @brief Retrieves the endmember partial entropies.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_partial_entropies()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return Partial entropies in [J/K].
+   */
+  Eigen::ArrayXd get_partial_entropies() const;
+
+  /**
+   * @brief Retrieves the second compositional derivative of the Gibbs free energy.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_gibbs_hessian()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return Second derivative of Gibbs in [J].
+   */
+  Eigen::MatrixXd get_gibbs_hessian() const;
+
+  /**
+   * @brief Retrieves the second compositional derivative of the entropy.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_entropy_hessian()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return Second derivative of entropy [J/K].
+   */
+  Eigen::MatrixXd get_entropy_hessian() const;
+
+  /**
+   * @brief Retrieves the second compositional derivative of the volume.
+   *
+   * Uses a cached value if available, or calls
+   * `Solution::compute_volume_hessian()` and caches the result.
+   *
+   * @note Use `Solution::reset()` to clear cached values.
+   *
+   * @return Second derivatives of volume in [m^3].
+   */
+  Eigen::MatrixXd get_volume_hessian() const;
+
  protected:
-  ;
+
+  // Overrides of defaults from Material
+  double compute_molar_internal_energy() const override;
+  double compute_molar_gibbs() const override;
+  double compute_molar_helmholtz() const override;
+  double compute_molar_mass() const override;
+  double compute_molar_volume() const override;
+  double compute_density() const override;
+  double compute_molar_entropy() const override;
+  double compute_molar_enthalpy() const override;
+  double compute_isothermal_bulk_modulus_reuss() const override;
+  double compute_isentropic_bulk_modulus_reuss() const override;
+  double compute_isothermal_compressibility_reuss() const override;
+  double compute_isentropic_compressibility_reuss() const override;
+  double compute_shear_modulus() const override;
+  double compute_p_wave_velocity() const override;
+  double compute_bulk_sound_velocity() const override;
+  double compute_shear_wave_velocity() const override;
+  double compute_grueneisen_parameter() const override;
+  double compute_thermal_expansivity() const override;
+  double compute_molar_heat_capacity_v() const override;
+  double compute_molar_heat_capacity_p() const override;
+
+  // New functions
+  /**
+   * @brief Computes molar excess gibbs free energy the of the solid solution.
+   *
+   * @return Excess gibbs free energy in [J/mol].
+   */
+  double compute_excess_gibbs() const;
+
+  /**
+   * @brief Computes excess molar volume of the solid solution.
+   *
+   * @return Excess volume in [m^3/mol].
+   */
+  double compute_excess_volume() const;
+
+  /**
+   * @brief Computes excess molar entropy of the solid solution.
+   *
+   * @return Excess entropy in [J/K/mol].
+   */
+  double compute_excess_entropy() const;
+
+  /**
+   * @brief Computes excess molar enthalpy of the solid solution.
+   *
+   * @return Excess enthalpy in [J/mol].
+   */
+  double compute_excess_enthalpy() const;
+
+  /**
+   * @brief Computes endmember activities.
+   *
+   * @return Endmember activities [unitless].
+   */
+  Eigen::ArrayXd compute_activities() const;
+
+  /**
+   * @brief Computes endmember activity coefficients of the solid solution.
+   *
+   * @return Endmember activity coefficients [unitless].
+   */
+  Eigen::ArrayXd compute_activity_coefficients() const;
+
+  /**
+   * @brief Computes excess partial molar gibbs free energies.
+   *
+   * @return Excess partial gibbs in [J/mol].
+   */
+  Eigen::ArrayXd compute_excess_partial_gibbs() const;
+
+  /**
+   * @brief Computes excess partial volumes of the solid solution.
+   *
+   * @return Excess partial volumes in [m^3/mol].
+   */
+  Eigen::ArrayXd compute_excess_partial_volumes() const;
+
+  /**
+   * @brief Computes excess partial entropies of the solid solution.
+   *
+   * @return Excess partial entropies in [J/K].
+   */
+  Eigen::ArrayXd compute_excess_partial_entropies() const;
+
+  /**
+   * @brief Computes endmember partial molar gibbs free energies.
+   *
+   * @return Partial molar gibbs free energies in [J/mol].
+   */
+  Eigen::ArrayXd compute_partial_gibbs() const;
+
+  /**
+   * @brief Computes endmember partial volumes of the solid solution.
+   *
+   * @return Partial volumes in [m^3].
+   */
+  Eigen::ArrayXd compute_partial_volumes() const;
+
+  /**
+   * @brief Computes endmember partial entropies.
+   *
+   * @return Partial entropies in [J/K].
+   */
+  Eigen::ArrayXd compute_partial_entropies() const;
+
+  /**
+   * @brief Hessian of gibbs free energy.
+   *
+   * @return Second compositional derivative [J].
+   */
+  Eigen::MatrixXd compute_gibbs_hessian() const;
+
+  /**
+   * @brief Hessian of entropy.
+   *
+   * @return Second compositional derivative [J/K].
+   */
+  Eigen::MatrixXd compute_entropy_hessian() const;
+
+  /**
+   * @brief Hessian of volumes.
+   *
+   * @return Second compositional derivative [m^3].
+   */
+  Eigen::MatrixXd compute_volume_hessian() const;
+
  private:
-  ;
+
+  mutable std::optional<double> excess_gibbs;
+  mutable std::optional<double> excess_volume;
+  mutable std::optional<double> excess_entropy;
+  mutable std::optional<double> excess_enthalpy;
+  // 1D Eigen arrays (could be made Vectors)
+  mutable std::optional<Eigen::ArrayXd> activities;
+  mutable std::optional<Eigen::ArrayXd> activity_coefficients;
+  mutable std::optional<Eigen::ArrayXd> excess_partial_gibbs;
+  mutable std::optional<Eigen::ArrayXd> excess_partial_volumes;
+  mutable std::optional<Eigen::ArrayXd> excess_partial_entropies;
+  mutable std::optional<Eigen::ArrayXd> partial_gibbs;
+  mutable std::optional<Eigen::ArrayXd> partial_volumes;
+  mutable std::optional<Eigen::ArrayXd> partial_entropies;
+  // 2D Matrices
+  mutable std::optional<Eigen::MatrixXd> gibbs_hessian;
+  mutable std::optional<Eigen::MatrixXd> entropy_hessian;
+  mutable std::optional<Eigen::MatrixXd> volume_hessian;
+  // site_occupancies - Map?
 
 };
 
