ae3add652edbdc0892e3c846755dc36fdcd33e7d
bjheinen <benedict.heinen@gmail.com>
2025-07-17 12:49:45 +0100

    Add test custom types (overloads)

---
+++ tests/utils/test_types.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2025 Benedict Heinen
+ *
+ * This file is part of burnman_cpp and is licensed under the
+ * GNU General Public License v3.0 or later. See the LICENSE file
+ * or <https://www.gnu.org/licenses/> for details.
+ *
+ * burnman_cpp is based on BurnMan: <https://geodynamics.github.io/burnman/>
+ */
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include "tolerances.hpp"
+#include "burnman/utils/types.hpp"
+
+TEST_CASE("FormulaMap addition and scalar multiplication", "[utils][types]") {
+  
+  FormulaMap fm1 {{"Si", 1.0}, {"O", 2.0}};
+  FormulaMap fm2 {{"Mg", 1.0}, {"O", 1.0}};
+
+  SECTION("Addition") {
+    FormulaMap result = fm1 + fm2;
+    REQUIRE(result["Si"] == 1.0);
+    REQUIRE(result["Mg"] == 1.0);
+    REQUIRE(result["O"] == 3.0);
+  }
+
+  SECTION("In-place addition") {
+    fm1 += fm2;
+    REQUIRE(fm1["Si"] == 1.0);
+    REQUIRE(fm1["Mg"] == 1.0);
+    REQUIRE(fm1["O"] == 3.0);
+  }
+
+  SECTION("Scalar multiplication") {
+    FormulaMap result = fm1 * 2.0;
+    REQUIRE(result["Si"] == 2.0);
+    REQUIRE(result["O"] == 4.0);
+  }
+
+  SECTION("Scalar multiplication (swapped order)") {
+    FormulaMap result = 3.0 * fm1;
+    REQUIRE(result["Si"] == 3.0);
+    REQUIRE(result["O"] == 6.0);
+  }
+
+  SECTION("In-place scalar multiplication") {
+    fm1 *= 2.5;
+    REQUIRE(fm1["Si"] == 2.5);
+    REQUIRE(fm1["O"] == 5.0);
+  }
+}
+
+// TODO: test excesses::Excesses (and move to types.hpp)
