498d5214703d70d7c58484bce77c80142d5448f3
bjheinen <benedict.heinen@gmail.com>
2025-07-17 13:59:31 +0100

    Add tests for logish

---
+++ tests/utils/test_math_utils.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2025 Benedict Heinen
+ *
+ * This file is part of burnman_cpp and is licensed under the
+ * GNU General Public License v3.0 or later. See the LICENSE file
+ * or <https://www.gnu.org/licenses/> for details.
+ *
+ * burnman_cpp is based on BurnMan: <https://geodynamics.github.io/burnman/>
+ */
+#include <cmath>
+#include <Eigen/Dense>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include "tolerances.hpp"
+#include "burnman/utils/math_utils.hpp"
+
+using namespace Catch::Matchers;
+
+TEST_CASE("logish is log for values > eps; 1D", "[utils][math_utils]") {
+  Eigen::ArrayXd x(3);
+  x << 1.0e-6, 1.0e-5, 1.0e-3; // All > eps = 1e-7
+  Eigen::ArrayXd expected = x.log();
+  Eigen::ArrayXd result = utils::logish(x);
+  CHECK(result.isApprox(expected, tol_rel));
+}
+
+TEST_CASE("logish is log for values > eps; 2D", "[utils][math_utils]") {
+  Eigen::ArrayXXd x(2, 2);
+  x <<
+    1.0e-6, 1.0e-5,
+    2.0e1, 1.0e-3; // All > eps = 1e-7
+  Eigen::ArrayXd expected = x.log();
+  Eigen::ArrayXd result = utils::logish(x);
+  CHECK(result.isApprox(expected, tol_rel));
+}
+
+TEST_CASE("logish uses Taylor expansion below eps", "[utils][math_utils]") {
+  Eigen::ArrayXd x(6);
+  x << 2.0e-8, 1.4e-9, 9.999e-8, 5.0e-13, 1.0e-35, 0;  // All <= eps (1.e-7)
+  Eigen::ArrayXd result = utils::logish(x);
+  double lim = std::log(1.0e-7) - 1.5;
+  // log(x) ~ logish(x) at x ~ eps
+  double ref_a = std::log(x[2]);
+  CHECK_THAT(result[2],
+    WithinRel(ref_a, tol_rel) || WithinAbs(ref_a, tol_abs));
+  // logish(x) same order of magnitude at near eps
+  CHECK(int(result[0]) == int(std::log(x[0])));
+  // logish levels
+  REQUIRE(int(std::log(x[1])) == -20);
+  CHECK(int(result[1]) == -17);
+  REQUIRE(int(std::log(x[3])) == -28);
+  CHECK(int(result[3]) == -17);
+  // log(x->0) -> inf; logish(x->0) -> lim (log(1e-7)-1.5)
+  CHECK_THAT(result[4],
+    WithinRel(lim, tol_rel) || WithinAbs(lim, tol_abs));
+  CHECK_THAT(result[5],
+    WithinRel(lim, tol_rel) || WithinAbs(lim, tol_abs));
+}
+
+TEST_CASE("logish - mixed range 2D", "[utils][math_utils]") {
+  Eigen::ArrayXXd x(2, 3);
+  x <<
+    2.0e-1, 1.4e-5, 9.99e-8,
+    5.0e-13, 1.0e1, 0;
+  Eigen::ArrayXXd result;
+  REQUIRE_NOTHROW(result = utils::logish(x));
+  REQUIRE(result.rows() == 2);
+  REQUIRE(result.cols() == 3);
+}
