4eb027ec57a7fb16263a81978fdf70a720b24e48
bjheinen <benedict.heinen@gmail.com>
2025-09-08 18:32:41 +0100

    Add solver loop to solve

--- src/tools/equilibration/equilibrate.cpp
+++ src/tools/equilibration/equilibrate.cpp
@@ -416,7 +416,7 @@ Eigen::MatrixXd J(
 }
 
 // TODO: EquilibrateResult or similar to hold output?
-? equilibrate(
+EquilibrateResult equilibrate(
   const FormulaMap& composition,
   Assemblage& assemblage,
   const ConstraintList& equality_constraints,
@@ -482,13 +482,6 @@ Eigen::MatrixXd J(
   EquilibrationParameters prm = get_equilibration_parameters(
     assemblage, composition, free_compositional_vectors);
 
-  // Set up solves constraint indices from ConstraintList
-  std::vector<std::size_t> nc;
-  nc.reserve(equality_constraints.size());
-  for (const ConstraintGroup& c_group : equality_constraints) {
-    nc.push_back(c_group.size());
-  }
-
   // Set default state if none
   double initial_pressure = 5.0e9;
   double initial_temperature = 1200.0;
@@ -516,10 +509,165 @@ Eigen::MatrixXd J(
   // Get parameter vector (x)
   Eigen::VectorXd parameter_vector = get_parameter_vector(assemblage, n_free_compositional_vectors);
 
-  // Set up combinations of constraints for the solve loop
+  // Set up solves constraint indices from ConstraintList
+  std::vector<std::size_t> grid_shape;
+  grid_shape.reserve(n_equality_constraints);
+  for (const ConstraintGroup& c_group : equality_constraints) {
+    grid_shape.push_back(c_group.size());
+  }
+  // Store strides for mapping back to grid_index
+  std::vector<std::size_t> strides = utils::compute_strides(grid_shape);
+  // Set up grid of solver results
+  NDArray<DampedNewtonResult> sol_array(grid_shape);
 
-  // Loop over problems and solve for each one
+  // Make solver settings
+  DampedNewtonSettings solver_settings;
+  solver_settings.store_iterates = store_iterates;
+  solver_settings.max_iterations = max_iterations;
+  solver_settings.tol = tol;
+  std::vector<int> embr_per_phase = assemblage.get_endmembers_per_phase();
+  solver_settings.lambda_bounds_func =
+    [embr_per_phase](const Eigen::VectorXd& dx, const Eigen::VectorXd& x) {
+      return lambda_bounds_func(dx, x, embr_per_phase);
+    };
+  // Make solver object
+  DampedNewtonSolver solver(solver_settings);
 
+  // Loop over all the problems
+  std::size_t n_problems = sol_array.size();
+  for (std::size_t problem_idx = 0; problem_idx < n_problems; ++problem_idx) {
+    // Get constraints idx and build current constraints
+    std::vector<std::size_t> constraint_indices = utils::map_index(problem_idx, strides);
+    ConstraintGroup problem_constraints;
+    problem_constraints.reserve(n_equality_constraints);
+    for (std::size_t i = 0; i < n_equality_constraints; ++i) {
+      // Clone constraints to have independent instances
+      // Could also use .get() and use raw pointers (need to modify F, J arg types)
+      problem_constraints.emplace_back(
+        equality_constraints[i][constraint_indices[i]]->clone());
+    }
+
+    // Note - mutates assemblage object (might need to clone before)
+    DampedNewtonResult sol = solver.solve(
+      parameter_vector,
+      [&](const Eigen::VectorXd& x) {
+        return F(
+          x,
+          assemblage,
+          problem_constraints,
+          prm.reduced_composition_vector,
+          prm.reduced_free_composition_vectors);
+      },
+      [&](const Eigen::VectorXd& x) {
+        return J(
+          x,
+          assemblage,
+          problem_constraints,
+          prm.reduced_free_composition_vectors);
+      },
+      {prm.constraint_matrix, prm.constraint_vector}
+    );
+
+    double maxres = 0.0;
+    if (sol.success && assemblage.get_reaction_affinities().size() > 0) {
+      // TODO: assemblage.get_reaction_affinities()
+      maxres = assemblage.get_reaction_affinities().cwiseAbs().maxCoeff() + 1.0e-5;
+      // TODO: assemblage.set_equilibrium_tolerance()
+      assemblage.set_equilibrium_tolerance(maxres);
+    }
+    // TODO: can't store assemblage as not separate object for each problem in loop
+    // Need to implement clone (might work) - see below
+    if (store_assemblage) {
+      // TODO!
+      // Clone with? sol.assemblage = assemblage.clone();
+      // Or better, make a new clone at start of each loop, need to be careful with
+      // lifetime as clone should return a pointer.
+      ;
+      // TODO: Maybe set equilibrium tolerance only here and not above?
+    }
+
+    // TODO: include iostream
+    if (verbose) {
+      std::cout << sol.message << std::endl;
+    }
+
+    // Store result!
+    sol_array(problem_idx) = std::move(sol);
+
+    if (problem_idx < n_problems - 1) {
+      // Get next set of constraints
+      std::vector<std::size_t> next_c_idx = utils::map_index(
+        problem_idx + 1, strides);
+      ConstraintGroup next_constraints;
+      next_constraints.reserve(n_equality_constraints);
+      for (std::size_t i = 0; i < n_equality_constraints; ++i) {
+        next_constraints.emplace_back(
+          equality_constraints[i][next_c_idx[i]]->clone());
+      }
+      // Get neighbouring indices in grid
+      std::vector<std::size_t> prev_indices;
+      for (std::size_t i = 0; i < n_equality_constraints; ++i) {
+        if (next_c_idx[i] != 0) {
+          std::vector<std::size_t> neighbor_idx = next_c_idx;
+          neighbor_idx[i] -= 1;
+          prev_indices.push_back(utils::flatten_index(neighbor_idx, strides));
+        }
+      }
+      bool updated_params = false;
+      for (std::size_t idx : prev_indices) {
+        const DampedNewtonResult& s = sol_array(idx);
+        if (s.success && !updated_params) {
+          // Next guess based on a Newton step using
+          // the old solution vector and Jacobian with new constraints
+          Eigen::VectorXd dF = F(
+            s.x,
+            assemblage,
+            next_constraints,
+            prm.reduced_composition_vector,
+            prm.reduced_free_composition_vectors
+          );
+          Eigen::PartialPivLU<Eigen::MatrixXd> luJ(s.J);
+          Eigen::VectorXd new_parameters = s.x + luJ.solve(-dF);
+          // Check constraints are satisfied
+          Eigen::VectorXd c = prm.constraint_matrix * new_parameters + prm.constraint_vector;
+          if ((c.array() <= 0.0).all()) {
+            // Constraints satisfied - accept new guess
+            parameters = new_parameters;
+          } else {
+            // Fallback to last parameter vector
+            parameters = s.x;
+            if (verbose) {
+              std::vector<std::string> exhausted_phases;
+              Eigen::ArrayXd phase_amounts = new_parameters(prm.phase_amount_indices);
+              for (Eigen::Index i = 0; i < phase_amounts.size(); ++i) {
+                if (phase_amounts(i) < 0.0) {
+                  // TODO: update to Eigen::Index
+                  exhausted_phases.push_back(
+                    assemblage.get_phase(i)->get_name()
+                  );
+                }
+              }
+              if (!exhausted_phases.empty()) {
+                std::string ex_p_message =
+                  "A phase might be exhausted before the next step: [";
+                for (const std::string& name : exhausted_phases) {
+                  ex_p_message += name + " ";
+                }
+                ex_p_message += "]";
+                std::cout << ex_p_message << std::endl;
+              }
+            } // exhausted phase check if verbose
+          } // parameter fallback
+          updated_params = true;
+        } // update param check
+      } // neighbouring constraints loop
+    } // not last problem check
+  } // loop over all problems
+
+  EquilibrateResult result;
+  result.sol_array = std::move(sol_array);
+  result.prm = std::move(prm);
+  return result;
 }
 
 // TODO: if we want to store assemblages like in the python then they need to be copyable:
