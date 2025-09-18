2e3b09d4f7eabe555533222d7e2369c2f39cd2bb
bjheinen <benedict.heinen@gmail.com>
2025-09-18 15:18:59 +0100

    Add failing placeholder tests for equilibrate routines

---
+++ tests/tools/equilibration/test_equilibrate_objective.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2025 Benedict Heinen
+ *
+ * This file is part of burnman_cpp and is licensed under the
+ * GNU General Public License v3.0 or later. See the LICENSE file
+ * or <https://www.gnu.org/licenses/> for details.
+ *
+ * burnman_cpp is based on BurnMan: <https://geodynamics.github.io/burnman/>
+ */
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include "burnman/tools/equilibration/equilibrate_objective.hpp"
+#include "tolerances.hpp"
+
+using namespace Catch::Matchers;
+using namespace burnman;
+
+TEST_CASE("equilibrate_objective", "[tools][equilibration]") {
+  FAIL("Tests need to be written!");
+}
