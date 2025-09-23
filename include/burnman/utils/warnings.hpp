eaded2f62e388a6ae76eedf296be6387ce005c8d
bjheinen <benedict.heinen@gmail.com>
2025-09-23 13:47:05 +0100

    Add helper function for printing warnings

---
+++ include/burnman/utils/warnings.hpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 Benedict Heinen
+ *
+ * This file is part of burnman_cpp and is licensed under the
+ * GNU General Public License v3.0 or later. See the LICENSE file
+ * or <https://www.gnu.org/licenses/> for details.
+ *
+ * burnman_cpp is based on BurnMan: <https://geodynamics.github.io/burnman/>
+ */
+#ifndef BURNMAN_UTILS_WARNINGS_HPP_INCLUDED
+#define BURNMAN_UTILS_WARNINGS_HPP_INCLUDED
+
+#include <iostream>
+#include <string>
+
+namespace burnman {
+  namespace utils {
+    /**
+     * @brief Prints a warning message.
+     *
+     * @param message The warning message to print.
+     */
+    inline void warn(const std::string& message) {
+      // Simple implementation: print to std::cerr
+      // Could switch to a logging framework if needed
+      std::cerr << "Warning: " << message << std::endl;
+    }
+  } // namespace utils
+} // namespace burnman
+
+#endif // BURNMAN_UTILS_WARNINGS_HPP_INCLUDED
