1cf7aa067dbf272f491787f617d9e899588f454d
bjheinen <benedict.heinen@gmail.com>
2025-09-23 15:48:08 +0100

    Add tests for utils::warn

---
+++ tests/utils/test_warnings.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2025 Benedict Heinen
+ *
+ * This file is part of burnman_cpp and is licensed under the
+ * GNU General Public License v3.0 or later. See the LICENSE file
+ * or <https://www.gnu.org/licenses/> for details.
+ *
+ * burnman_cpp is based on BurnMan: <https://geodynamics.github.io/burnman/>
+ */
+#include <catch2/catch_test_macros.hpp>
+#include "burnman/utils/warnings.hpp"
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace burnman;
+
+TEST_CASE("Validate warnings", "[utils][warnings]") {
+  // Redirect cerr to grab output
+  std::streambuf* orig_cerr = std::cerr.rdbuf();
+  std::ostringstream oss;
+  std::cerr.rdbuf(oss.rdbuf());
+  // Generate a warning
+  utils::warn("This is a test warning!");
+  REQUIRE(oss.str() == "Warning: This is a test warning!\n");
+  // Restore cerr buffer
+  std::cerr.rdbuf(orig_cerr);
+}
